5ebb977c9cf6e952ae146d7e9c1e9fa1824f3176
Sebastiaan Benjamins <45468731+cbasje@users.noreply.github.com>
2022-09-12 21:45:05 +0200

    Add netlify support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,8 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import netlify from "@astrojs/netlify/edge-functions"; // TODO: does edge work well?
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    output: "server",
+    adapter: netlify(),
+});
