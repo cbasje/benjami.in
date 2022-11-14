de6ab0093af93be624f94924da8b6c3061a71f0d
Sebastiaan Benjamins <45468731+cbasje@users.noreply.github.com>
2022-11-14 12:09:19 +0100

    Adding cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import { defineConfig } from "astro/config";
-
-import netlify from "@astrojs/netlify/functions";
+import cloudflare from "@astrojs/cloudflare";
 import sanity from "astro-sanity";
 
 // https://astro.build/config
 export default defineConfig({
     output: "server",
-    adapter: netlify(),
+    adapter: cloudflare(),
     integrations: [
         sanity({
             projectId: "jt1bq8i3",
