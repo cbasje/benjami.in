bc8f7f09ceab0e942d340829bc7a04da9cf98ba9
Sebastiaan Benjamins <45468731+cbasje@users.noreply.github.com>
2022-11-14 18:06:50 +0100

    Added astro-i18next

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from "astro/config";
 import sanity from "astro-sanity";
+import astroI18next from "astro-i18next";
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,5 +12,6 @@ export default defineConfig({
             apiVersion: "2021-03-25",
             useCdn: true,
         }),
+        astroI18next(),
     ],
 });
