e5e1a9d935724618e6daaac9fb7a33169667f484
Sebastiaan Benjamins <45468731+cbasje@users.noreply.github.com>
2022-11-14 12:25:18 +0100

    Add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import { defineConfig } from "astro/config";
-
-import netlify from "@astrojs/netlify/functions";
+import vercel from "@astrojs/vercel/serverless";
 import sanity from "astro-sanity";
 
 // https://astro.build/config
 export default defineConfig({
     output: "server",
-    adapter: netlify(),
+    adapter: vercel(),
     integrations: [
         sanity({
             projectId: "jt1bq8i3",
