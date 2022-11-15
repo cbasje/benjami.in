7b6adf61e850c80f20685660cdcc3dd054872575
Sebastiaan Benjamins <45468731+cbasje@users.noreply.github.com>
2022-11-15 14:21:56 +0100

    Add astro-seo + astro-fonts-next

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
-import { defineConfig } from "astro/config";
-import sanity from "astro-sanity";
+import fonts from "astro-fonts-next";
 import astroI18next from "astro-i18next";
+import sanity from "astro-sanity";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
@@ -13,5 +14,10 @@ export default defineConfig({
             useCdn: true,
         }),
         astroI18next(),
+        fonts({
+            url: [
+                "https://fonts.googleapis.com/css2?family=Inter:wght@400%253B500%253B700&family=Space+Mono:ital,wght@0,400%253B0,700%253B1,400%253B1,700&display=swap",
+            ],
+        }),
     ],
 });
