12876ee7680a2990c690595c38a7006d2b453aea
Sebastiaan Benjamins <45468731+cbasje@users.noreply.github.com>
2022-11-15 22:47:12 +0100

    Add @stitches/core + move styling over

---
+++ src/pages/api/now-playing.ts
@@ -0,0 +1,43 @@
+import type { Handler } from "@netlify/functions";
+import { getNowPlaying } from "../../../lib/now-playing";
+import type { SpotifyData } from "../../../lib/types";
+
+const parseName = (name: string): string => {
+    return name.split(/[(-]/i)[0];
+};
+const parseArtist = (artists: any[]): string => {
+    return artists.map((a: any) => a.name)[0] ?? "";
+};
+
+const handler: Handler = async (event, context) => {
+    const response = await getNowPlaying();
+
+    if (!response.ok || response.status === 204 || response.status >= 400) {
+        return {
+            statusCode: 200,
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+                isPlaying: false,
+            }),
+        };
+    }
+
+    const song: any = await response.json();
+
+    const data: SpotifyData = {
+        isPlaying: song.is_playing,
+        title: parseName(song.item.name),
+        artist: parseArtist(song.item.artists),
+        // album: parseName(song.item.album.name),
+        albumImage: song.item.album.images[2],
+        songUrl: song.item.external_urls.spotify,
+    };
+
+    return {
+        statusCode: 200,
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ ...data }),
+    };
+};
+
+export { handler };
