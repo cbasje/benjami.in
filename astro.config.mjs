43990fc5f26f62849fd3926be0cb330b9449252b
Sebastiaan Benjamins <45468731+cbasje@users.noreply.github.com>
2022-11-15 23:02:25 +0100

    Fix google font

--- astro.config.mjs
+++ astro.config.mjs
@@ -17,7 +17,7 @@ export default defineConfig({
         astroI18next(),
         fonts({
             url: [
-                "https://fonts.googleapis.com/css2?family=Inter:wght@400%253B500%253B700&family=Space+Mono:ital,wght@0,400%253B0,700%253B1,400%253B1,700&display=swap",
+                "https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&family=Space+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap",
             ],
         }),
         react(),
