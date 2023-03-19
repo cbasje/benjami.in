accd56d37c2b3f5e714d19fa4c6d900b28e86539
Sebastiaan Benjamins <45468731+cbasje@users.noreply.github.com>
2023-03-19 17:25:07 +0100

    Add Svelte

---
+++ src/components/SpotifyPlayerWrapper.svelte
@@ -0,0 +1,15 @@
+<script>
+    import {
+        QueryClient,
+        QueryClientProvider,
+    } from "@sveltestack/svelte-query";
+    import SpotifyPlayer from "./SpotifyPlayer.svelte";
+
+    const queryClient = new QueryClient();
+</script>
+
+<div>
+    <QueryClientProvider client={queryClient}>
+        <SpotifyPlayer />
+    </QueryClientProvider>
+</div>
