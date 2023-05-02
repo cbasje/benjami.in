bb7235dc70d273990d3e293135350e8797b8c101
Sebastiaan Benjamins <45468731+cbasje@users.noreply.github.com>
2023-05-02 17:56:58 +0200

    Add astro/image

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
+import image from "@astrojs/image";
 import svelte from "@astrojs/svelte";
 import astroI18next from "astro-i18next";
 import sanity from "astro-sanity";
 import { defineConfig } from "astro/config";
-import image from "@astrojs/image";
 
-// https://astro.build/config
 export default defineConfig({
     output: "static",
     site: "https://benjami.in",
@@ -16,6 +15,9 @@ export default defineConfig({
             useCdn: true,
         }),
         astroI18next(),
+        // image({
+        //     serviceEntryPoint: "@astrojs/image/sharp",
+        // }),
         image(),
         svelte(),
     ],
