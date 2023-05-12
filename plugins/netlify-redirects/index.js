6585d2d10f2c7253ff7c92da7f58a1c8a0574f6b
Sebastiaan Benjamins <45468731+cbasje@users.noreply.github.com>
2023-05-12 17:44:58 +0200

    Update index.js

--- plugins/netlify-redirects/index.js
+++ plugins/netlify-redirects/index.js
@@ -31,17 +31,23 @@ module.exports = {
     onPostBuild: ({ constants }) => {
         console.log("Attempting to append _redirects to dist/_redirects...");
         fs.readdir("/opt/build/repo/src/pages", "utf-8", function (err, data) {
-            console.log("🚀 ~ data:", data);
-
             if (err) {
                 console.log(
                     "Failed to read redirects, do you have a '_redirects' file in the root of your project?"
                 );
                 throw err;
             }
+
+            const red = data
+                .filter((f) => f.endsWith(".astro"))
+                .map((f) => {
+                    const route = f.replace(".astro", "");
+                    return `/${route} /nl/${route} 301 Language=nl\n`;
+                });
+
             fs.appendFile(
                 `/opt/build/repo/${constants.PUBLISH_DIR}/_redirects`,
-                "\n" + data,
+                red,
                 function (err) {
                     if (err) {
                         console.log("Fail");
