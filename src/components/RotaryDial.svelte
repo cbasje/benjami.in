b2e89ea273fded9341c4b21ec7e63ef696c74353
Sebastiaan Benjamins <45468731+cbasje@users.noreply.github.com>
2023-05-12 21:24:38 +0200

    Update RotaryDial.svelte

--- src/components/RotaryDial.svelte
+++ src/components/RotaryDial.svelte
@@ -282,6 +282,9 @@
                 background: var(--background);
 
                 transition: opacity 140ms cubic-bezier(0.25, 0.46, 0.45, 0.94);
+
+                pointer-events: none;
+                user-select: text;
             }
 
             :global(li a) {
