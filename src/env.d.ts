c0763a9b4921ecb54e107ec72d5010a28547dcfc
Sebastiaan Benjamins <45468731+cbasje@users.noreply.github.com>
2023-05-14 12:12:52 +0200

    Update env.d.ts

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,16 @@
 /// <reference types="astro/client" />
 /// <reference path="../.astro-i18n/generated.d.ts" />
+
+interface ImportMetaEnv {
+    readonly SANITY_STUDIO_PROJECT_ID: string;
+    readonly SANITY_STUDIO_DATASET: string;
+
+    readonly SPOTIFY_CLIENT_ID: string;
+    readonly SPOTIFY_CLIENT_SECRET: string;
+    readonly SPOTIFY_CODE: string;
+    readonly SPOTIFY_REFRESH_TOKEN: string;
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv;
+}
