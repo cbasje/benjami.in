012daf1c248d08fdc3796ae0d89e7af95db2e48e
Sebastiaan Benjamins <sebastiaan@benjami.in>
2023-10-30 00:38:30 +0100

    Try static routing

--- astro.config.ts
+++ astro.config.ts
@@ -1,12 +1,8 @@
 import svelte from "@astrojs/svelte";
 import { defineConfig } from "astro/config";
-import node from "@astrojs/node";
 
 export default defineConfig({
-    output: "server",
-    adapter: node({
-        mode: "standalone",
-    }),
+    output: "static",
     site: "https://benjami.in",
     integrations: [svelte()],
 });
