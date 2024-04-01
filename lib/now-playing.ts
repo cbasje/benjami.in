01741e5f5f9b5367bbc1a623db131e68903dcf37
Sebastiaan Benjamins <sebastiaan@benjami.in>
2024-04-01 11:58:03 +0200

    Remove node-fetch import

--- lib/now-playing.ts
+++ lib/now-playing.ts
@@ -1,5 +1,3 @@
-import fetch from "node-fetch";
-
 const client_id = import.meta.env.SPOTIFY_CLIENT_ID;
 const client_secret = import.meta.env.SPOTIFY_CLIENT_SECRET;
 const refresh_token = import.meta.env.SPOTIFY_REFRESH_TOKEN;
