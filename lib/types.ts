28602deece8182be8476fd4542c58f1a23b31040
Sebastiaan Benjamins <sebastiaan@benjami.in>
2024-06-11 00:27:22 +0200

    Remove old /api

--- lib/types.ts
+++ lib/types.ts
@@ -1,12 +1,3 @@
-export interface SpotifyData {
-    isPlaying: boolean;
-    title?: string;
-    artist?: string;
-    album?: string;
-    albumImage?: AlbumImage;
-    songUrl?: string;
-}
-
 interface AlbumImage {
     url: string;
     width: number;
