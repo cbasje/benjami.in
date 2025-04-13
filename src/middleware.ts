2c49e9ddfd747ea5f612382a33359838de842af4
Sebastiaan Benjamins <sebastiaan@benjami.in>
2025-04-13 20:04:30 +0200

    Remove Umami API

--- src/middleware.ts
+++
@@ -1,7 +0,0 @@
-import { defineMiddleware } from "astro:middleware";
-import { track } from "./lib/umami";
-
-export const onRequest = defineMiddleware((context, next) => {
-  track("visit", { url: context.url.pathname });
-  return next();
-});
