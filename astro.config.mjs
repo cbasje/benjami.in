e4f9c7567fce2a86cd5eec38ea1a9f9ae73e1608
Sebastiaan Benjamins <sebastiaan@benjami.in>
2025-04-13 19:40:02 +0200

    Add Partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,60 +1,63 @@
-import { defineConfig } from "astro/config";
+import { defineConfig, envField } from "astro/config";
 
+import partytown from "@astrojs/partytown";
 import deno from "@deno/astro-adapter";
 import icon from "astro-icon";
+
 import browserslist from "browserslist";
 import { browserslistToTargets } from "lightningcss";
 
 // https://astro.build/config
 export default defineConfig({
-    output: "server",
-    adapter: deno({
-        hostname: "0.0.0.0",
-        port: 3000,
+  output: "server",
+  adapter: deno({
+    hostname: "0.0.0.0",
+    port: 3000,
+  }),
+
+  integrations: [
+    partytown(),
+    icon({
+      include: {
+        ph: ["*"],
+      },
+      svgoOptions: {
+        plugins: [
+          {
+            name: "convertColors",
+            params: {
+              currentColor: true,
+            },
+          },
+        ],
+      },
     }),
+  ],
 
-    integrations: [
-        icon({
-            include: {
-                ph: ["*"],
-            },
-            svgoOptions: {
-                plugins: [
-                    {
-                        name: "convertColors",
-                        params: {
-                            currentColor: true,
-                        },
-                    },
-                ],
-            },
-        }),
-    ],
+  site: "https://benjami.in",
 
-    site: "https://benjami.in",
-
-    vite: {
-        css: {
-            transformer: "lightningcss",
-            lightningcss: {
-                targets: browserslistToTargets(browserslist(">= 0.25%")),
-                drafts: {
-                    customMedia: true,
-                },
-            },
+  vite: {
+    css: {
+      transformer: "lightningcss",
+      lightningcss: {
+        targets: browserslistToTargets(browserslist(">= 0.25%")),
+        drafts: {
+          customMedia: true,
         },
-        build: {
-            cssMinify: "lightningcss",
-        },
+      },
     },
-
-    i18n: {
-        defaultLocale: "en",
-        locales: ["nl", "en"],
-        routing: {
-            prefixDefaultLocale: true,
-        },
+    build: {
+      cssMinify: "lightningcss",
     },
+  },
+
+  i18n: {
+    defaultLocale: "en",
+    locales: ["nl", "en"],
+    routing: {
+      prefixDefaultLocale: true,
+    },
+  },
 
   env: {
     schema: {
